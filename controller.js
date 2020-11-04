6e4f3e0b2e2d532d9eff119f83c646062910463a
Alex Bean <alexbean95@gmail.com>
2020-11-04 12:25:12 -0700

    week 5 day 3 lecture

---
+++ controller.js
@@ -0,0 +1,24 @@
+module.exports = {
+    getPlanes: (req, res) => {
+        const db = req.app.get('db');
+        db.get_planes()
+        .then ( plane => {
+            res.status(200).send(planes)
+        }).catch(err => {
+            console.log(err);
+            res.status(404).send("Couldn't find what you were looking for")
+        })  
+    },
+
+    addPlane: (req, res) => {
+        const db = req.app.get('db');
+        const {type, count} = req.body;
+        db.add_plane([type, count])
+        .then ( planes => {
+            res.status(200).send(planes)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send("Could not add plane")
+        })
+    }
+}
